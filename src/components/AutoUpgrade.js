55e1e51c421228ac2e800342278d2ea0e82c2488
Joseph Karnafel <josephkarnafel@gmail.com>
2020-12-22 15:58:45 -0800

    begin adding multiple types of auto upgrades and styling

--- src/components/AutoUpgrade.js
+++ src/components/AutoUpgrade.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledP = styled.p`
+  margin: 1px;
+  font-size: 15px;
+`;
+
+const ComponentStyleWrapper = styled.section`
+  border-style: solid;
+  border-color: rgba(255, 255, 255, 0.3);
+`
 
 function AutoUpgrade(props) {
   return (
-    <h2>Auto Upgrades</h2>
+    <ComponentStyleWrapper>
+      <h4>{props.name}</h4>
+      <StyledP>+{props.bytesPerSecond} per second</StyledP>
+      <StyledP>Cost: {props.cost}</StyledP>
+    </ComponentStyleWrapper>
   )
 }
 
-export default AutoUpgradesPanel;
+AutoUpgrade.propTypes = {
+  name: PropTypes.string,
+  bytesPerSecond: PropTypes.number,
+  cost: PropTypes.number,
+  description: PropTypes.string
+}
+
+export default AutoUpgrade;
--- src/components/AutoUpgradesPanel.js
+++ src/components/AutoUpgradesPanel.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import AutoUpgrade from './AutoUpgrade';
 
 function AutoUpgradesPanel(props) {
   return (
-    <h2>Auto Upgrades</h2>
+    <>
+      <h2>Auto Upgrades</h2>
+      <AutoUpgrade name='Spacebar Weight' bytesPerSecond='1 bytes' cost='15 bytes'/>
+    </>
   )
 }
 
